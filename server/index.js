da5dea91adb2df138c5bc22395f26b4cb644a006
Alexander.D <74902666+lonegdd@users.noreply.github.com>
2023-05-22 18:25:28 +0300

    feat - run server

--- server/index.js
+++ server/index.js
@@ -1,4 +1,7 @@
 const express = require('express')
+require('dotenv').config()
 const port = process.PORT || 5000
 
 const app = express()
+
+app.listen(port, console.log(`Server running on port ${port}`))

1ae0d4421e22a6228edb3d2e9cdf6ce57726e6c9
Alexander.D <74902666+lonegdd@users.noreply.github.com>
2023-05-24 00:22:37 +0300

    feat(Header) - add component header for display view header

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,21 @@
+import {ApolloProvider, ApolloClient, InMemoryCache} from '@apollo/client'
+import {Header} from "./components/Header";
+import {Clients} from "./components/Clients";
 
-function App() {
-  return (
-    <div className="container"><h1>Hello World</h1>
-    </div>
-  );
+const client = new ApolloClient({
+    uri: 'http://localhost:5000/graphql',
+    cache: new InMemoryCache(),
+})
+
+export const App = () => {
+    return (
+        <>
+            <ApolloProvider client={client}>
+                <Header/>
+                <Clients/>
+            </ApolloProvider>
+        </>
+    )
 }
 
-export default App;
+

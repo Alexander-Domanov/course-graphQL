b09d79191ae359a3dc90add98600ee0b86a8af62
Alexander.D <74902666+lonegdd@users.noreply.github.com>
2023-05-24 00:55:07 +0300

    feat(Client) - display view clients table. getClients on server

--- client/src/components/Clients.jsx
+++ client/src/components/Clients.jsx
@@ -1,8 +1,29 @@
+import {gql, useQuery} from "@apollo/client";
+
+const GET_CLIENTS = gql`
+ query getClients {
+    clients {
+      id
+      name
+      email
+      phone
+    }
+  }
+`
 
 export const Clients = () => {
+    const {loading, error, data} = useQuery(GET_CLIENTS)
+    if (loading) return <div>Loading...</div>
+    if (error) return <div>Error: {error.message}</div>
     return (
         <div>
-            Clients
+            {data && data.clients.map(client => (
+                <div key={client.id}>
+                    <p>{client.name}</p>
+                    <p>{client.email}</p>
+                    <p>{client.phone}</p>
+                </div>
+            ))}
         </div>
     )
 }

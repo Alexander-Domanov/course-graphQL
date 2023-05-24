e7e5af00777913519707bd3c80e9b815508ab34a
Alexander.D <74902666+lonegdd@users.noreply.github.com>
2023-05-24 22:27:46 +0300

    feat(spinner) - add spinner component. Use him on Clients

--- client/src/components/Clients.jsx
+++ client/src/components/Clients.jsx
@@ -1,29 +1,34 @@
-import {gql, useQuery} from "@apollo/client";
+import {useQuery} from "@apollo/client";
+import {ClientRow} from "./ClientRow";
+import {GET_CLIENTS} from "../queries/clientQueries";
+import {Spinner} from "./Spinner";
 
-const GET_CLIENTS = gql`
- query getClients {
-    clients {
-      id
-      name
-      email
-      phone
-    }
-  }
-`
 
 export const Clients = () => {
     const {loading, error, data} = useQuery(GET_CLIENTS)
-    if (loading) return <div>Loading...</div>
-    if (error) return <div>Error: {error.message}</div>
+
+    if (loading) return <Spinner/>;
+    if (error) return <p>Something Went Wrong</p>;
+
     return (
         <div>
-            {data && data.clients.map(client => (
-                <div key={client.id}>
-                    <p>{client.name}</p>
-                    <p>{client.email}</p>
-                    <p>{client.phone}</p>
-                </div>
-            ))}
+            {!loading && !error && (
+                <table className='table table-hover mt-3'>
+                    <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Phone</th>
+                        <th></th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {data.clients.map((client) => (
+                        <ClientRow key={client.id} client={client} />
+                    ))}
+                    </tbody>
+                </table>
+            )}
         </div>
     )
 }

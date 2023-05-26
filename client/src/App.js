e58009bac3a2ab756727f2b3a2c09426bde4b61b
Alexander.D <74902666+lonegdd@users.noreply.github.com>
2023-05-26 21:47:03 +0300

    feat - add pages(Home,NotFound,Project) implemented the transition on the stairs

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,9 @@
-import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
+import {ApolloProvider, ApolloClient, InMemoryCache} from '@apollo/client';
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import Home from "./pages/Home";
+import {NotFound} from "./pages/NotFound";
 import {Header} from "./components/Header";
-import {Clients} from "./components/Clients";
-import {AddClientModal} from "./components/AddClientModal";
-import {Projects} from "./components/Projects";
-
+import {Project} from "./pages/Project";
 
 
 // existing - incoming data. incoming - returned data
@@ -35,10 +35,16 @@ export const App = () => {
     return (
         <>
             <ApolloProvider client={client}>
-                <Header/>
-                <AddClientModal/>
-                <Projects/>
-                <Clients/>
+                <Router>
+                    <Header />
+                    <div className='container'>
+                        <Routes>
+                            <Route path='/' element={<Home />} />
+                            <Route path='/projects/:id' element={<Project />} />
+                            <Route path='*' element={<NotFound />} />
+                        </Routes>
+                    </div>
+                </Router>
             </ApolloProvider>
         </>
     )
